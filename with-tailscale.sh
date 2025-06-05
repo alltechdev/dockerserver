6957d7a88d6c2aaef3317ea0d1d636932c2722a0
alltechdev <github@alltech.dev>
2025-06-05 13:07:44 -0400

    Create with-tailscale.sh
    standard-install.sh with added Tailscale installation

---
+++ with-tailscale.sh
@@ -0,0 +1,8 @@
+sudo apt update && sudo apt upgrade -y
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+sudo mkdir -p /opt/stacks /opt/dockge
+cd /opt/dockge
+sudo curl https://raw.githubusercontent.com/louislam/dockge/master/compose.yaml --output compose.yaml
+sudo docker compose up -d
+curl -fsSL https://tailscale.com/install.sh | sh
