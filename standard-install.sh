0bf0a7c0124d7c343b9dfa24f002d7d5e25b8f83
alltechdev <github@alltech.dev>
2025-06-05 12:55:28 -0400

    Update standard-install.sh

--- standard-install.sh
+++ standard-install.sh
@@ -3,5 +3,5 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 sudo mkdir -p /opt/stacks /opt/dockge
 cd /opt/dockge
-curl https://raw.githubusercontent.com/louislam/dockge/master/compose.yaml --output compose.yaml
-docker compose up -d
+sudo curl https://raw.githubusercontent.com/louislam/dockge/master/compose.yaml --output compose.yaml
+sudo docker compose up -d
